e7d94894ead44824115f9f805e93693a206f50d6
btcmachineordinals <125616647+btcmachineordinals@users.noreply.github.com>
2023-02-18 09:34:48 -0500

    Update bulk-ordinal-inscription-advanced.py

--- bulk-ordinal-inscription-advanced.py
+++ bulk-ordinal-inscription-advanced.py
@@ -1,55 +1,38 @@
 import subprocess
 import json
-import time
 import os
 
-# Parameters
-num_inscriptions = 100
-start_num = 1
-batch_size = 10
-fee_rate = 20.0
-
-# File to store the inscriptions
-inscriptions_file = 'operators-inscriptions.json'
-
-# Create empty inscriptions file or read existing file
-if os.path.exists(inscriptions_file):
-    with open(inscriptions_file, 'r') as f:
-        inscriptions = json.load(f)
+# Check if the JSON file exists, if not, create an empty dictionary
+if os.path.isfile('operators.json'):
+    with open('operators.json', 'r') as f:
+        try:
+            operators = json.load(f)
+        except json.decoder.JSONDecodeError:
+            operators = {}
 else:
-    inscriptions = []
+    operators = {}
 
-# Keep track of the last successful inscription number
-last_successful_inscription = start_num - 1
+# Find the last index of the operators dictionary and start from there
+last_index = max([int(i) for i in operators.keys()], default=0)
 
-# Loop through batches of inscriptions
-while last_successful_inscription < num_inscriptions - 1:
-    # Create a list of subprocesses to run in parallel
-    processes = []
-    for i in range(last_successful_inscription + 1, min(last_successful_inscription + 1 + batch_size, num_inscriptions + 1)):
-        image_path = f'build/images/{i}.png'
-        cmd = [r'F:\CORE\ORD\ord', '--cookie-file', r'F:\CORE\.cookie', 'wallet', 'inscribe', image_path, '--fee-rate', str(fee_rate)]
-        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        processes.append((p, i))
+# List of image paths to inscribe
+image_paths = ["C:\\Users\\Kevin\\Documents\\Render\\BTCMACHINES\\operators\\build\\images\\{}.png".format(i) for i in range(110, 200)]
 
-    # Wait for all subprocesses to finish
-    for p, i in processes:
-        p.wait()
+# Loop through the images and inscribe them
+for index, image_path in enumerate(image_paths):
+    # Inscribe the image
+    subprocess.run(['F:\\CORE\\ORD\\ord', '--cookie-file', 'F:\\CORE\\.cookie', 'wallet', 'inscribe', image_path, '--fee-rate', '9.0'])
+    
+    # Get the output from the command
+    result = subprocess.run(['F:\\CORE\\ORD\\ord', '--cookie-file', 'F:\\CORE\\.cookie', 'wallet', 'show', 'inscription', str(index + last_index + 1)], capture_output=True)
+    
+    # Decode the output and add it to the operators dictionary
+    inscription = result.stdout.decode('utf-8').strip()
+    operators[str(index + last_index + 1)] = inscription
 
-        # Check if inscription was successful
-        if p.returncode == 0:
-            print(f"Inscription {i} successful.")
-            inscriptions.append(f'Inscription {i}')
-            last_successful_inscription = i
+# Sort the operators dictionary by index
+sorted_operators = dict(sorted(operators.items(), key=lambda x: int(x[0])))
 
-        # If there was an error, print the error message and exit
-        else:
-            print(f"Error: Inscription {i} failed with message:\n{p.stderr.read().decode()}")
-            exit()
-
-    # Wait for a bit before starting the next batch of inscriptions
-    time.sleep(10)
-
-# Write inscriptions to file
-with open(inscriptions_file, 'w') as f:
-    json.dump(inscriptions, f, indent=4)
+# Write the sorted operators dictionary to the JSON file
+with open('operators.json', 'w') as f:
+    json.dump(sorted_operators, f, indent=4)
